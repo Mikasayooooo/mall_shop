95dbc4fe1a916b0ab2d7da2b7b968464eb85f560
lhl <1056205431@qq.com>
2020-11-15 13:19:31 +0800

    创建users子应用 & 自定义用户模型 & 追加导包路径 & 修改认证模型类 & 迁移建表

--- mall/mall/settings/dev.py
+++ mall/mall/settings/dev.py
@@ -10,13 +10,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, sys
+
+# os.path: 拼接路径
+# sys.path: 查询导包路径的
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # 当前文件的名字的上一级目录的上一级目录
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print('BASE_DIR---------------->', BASE_DIR)
 # /home/python/tools/pyProjects/djangoStudy/djangoProjects/mall_shop/mall/mall
+
+print('*' * 30)
+
+print(sys.path)
+# ['/home/python/tools/pyProjects/djangoStudy/djangoProjects/mall_shop/mall',......]
+# mall.apps.users.apps.UsersConfig
+
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+#  追加系统的导包路径(目的：1.注册子应用时，可以写的方便点  2.修改django认证模型时，必须以应用名.模型名)
+
+print('*' * 30)
+
+print(sys.path)
+# ['/home/python/tools/pyProjects/djangoStudy/djangoProjects/mall_shop/mall/mall/apps',......]
+# users.apps.UsersConfig
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +58,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'  # DRF
+    'rest_framework',  # DRF
+    'users.apps.UsersConfig'  # 用户模块
 ]
 
 MIDDLEWARE = [
@@ -192,7 +211,12 @@ LOGGING = {
     }
 }
 
+# DRF配置项
 REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'mall.utils.exceptions.exception_handler',
 }
+
+# 修改Django认证系统的用户模型类
+# mall.apps.users.models.User
+AUTH_USER_MODEL = 'users.User'  # 应用名.模型名
