ad11906f5c99f9483d2a3ef4de88cd6a1da309dc
lhl <1056205431@qq.com>
2020-11-16 09:47:02 +0800

    JWT登录视图01

--- mall/mall/apps/users/urls.py
+++ mall/mall/apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
 
@@ -9,4 +10,7 @@ urlpatterns = [
     url(r'^username/(?P<username>\w{5,20})/count/$', views.UsernameView.as_view()),
     # 判断手机号是否已经注册
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+    # JWT登录
+    url(r'^authorizations/$', obtain_jwt_token),   # 仅仅生成token,认证是属于django的,只是封装了一下
+    #     obtain_jwt_token = ObtainJSONWebToken.as_view()
 ]
