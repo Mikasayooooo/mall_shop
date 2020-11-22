629266a5fae0662820e4f4790ff25a0bdcc9e825
lhl <1056205431@qq.com>
2020-11-22 17:08:11 +0800

    商品列表界面静态化celery及监听admin数据变化

--- mall/mall/apps/contents/crons.py
+++ mall/mall/apps/contents/crons.py
@@ -6,6 +6,7 @@ import time
 
 from goods.models import GoodsChannel
 from .models import ContentCategory
+from mall.apps.goods.utils import get_categories
 
 
 # 用来调试
@@ -29,28 +30,33 @@ def generate_static_index_html():
     #
     #     }
     # }
-    categories = OrderedDict()
-    channels = GoodsChannel.objects.order_by('group_id', 'sequence')
-    for channel in channels:
-        group_id = channel.group_id  # 当前组
 
-        if group_id not in categories:
-            categories[group_id] = {'channels': [], 'sub_cats': []}
 
-        cat1 = channel.category  # 当前频道的类别
+    # categories = OrderedDict()
+    # channels = GoodsChannel.objects.order_by('group_id', 'sequence')
+    # for channel in channels:
+    #     group_id = channel.group_id  # 当前组
+    #
+    #     if group_id not in categories:
+    #         categories[group_id] = {'channels': [], 'sub_cats': []}
+    #
+    #     cat1 = channel.category  # 当前频道的类别
+    #
+    #     # 追加当前频道
+    #     categories[group_id]['channels'].append({
+    #         'id': cat1.id,
+    #         'name': cat1.name,
+    #         'url': channel.url
+    #     })
+    #     # 构建当前类别的子类别
+    #     for cat2 in cat1.goodscategory_set.all():
+    #         cat2.sub_cats = []
+    #         for cat3 in cat2.goodscategory_set.all():
+    #             cat2.sub_cats.append(cat3)
+    #         categories[group_id]['sub_cats'].append(cat2)
 
-        # 追加当前频道
-        categories[group_id]['channels'].append({
-            'id': cat1.id,
-            'name': cat1.name,
-            'url': channel.url
-        })
-        # 构建当前类别的子类别
-        for cat2 in cat1.goodscategory_set.all():
-            cat2.sub_cats = []
-            for cat3 in cat2.goodscategory_set.all():
-                cat2.sub_cats.append(cat3)
-            categories[group_id]['sub_cats'].append(cat2)
+    categories = get_categories()
+
 
     # 广告内容
     contents = {}
--- mall/mall/apps/goods/admin.py
+++ mall/mall/apps/goods/admin.py
@@ -1,9 +1,71 @@
 from django.contrib import admin
 
 from . import models
+from celery_tasks.html.tasks import generate_static_list_search_html
 
-admin.site.register(models.GoodsCategory)
-admin.site.register(models.GoodsChannel)
+
+# 修改的是商品类别表里的数据
+# 模型站点管理类不仅可以 设置admin界面的样式,还可以监听admin界面的事件
+class GoodsCategoryAdmin(admin.ModelAdmin):
+    '''商品类别模型站点管理类'''
+    def save_model(self, request, obj, form, change):
+        '''
+        当点击admin的保存按钮时会调用此方法
+        :param request: 保存时本次的请求对象
+        :param obj: 本次要保存的模型对象
+        :param form: admin中表单
+        :param change: 是否改为
+        :return:
+        '''
+
+        obj.save()
+
+        # 重新生成新的列表静态界面
+        generate_static_list_search_html.delay()
+        # 先生成静态页面,后修改数据的话,永远慢一步, 必须先改数据,后生成
+
+        # 需要使用celery
+        # import time
+        # time.sleep(5)
+
+    def delete_model(self, request, obj):
+        '''
+        当点击admin的删除按钮时会调用此方法
+        :param request: 保存时本次的请求对象
+        :param obj: 本次要保存的模型对象
+        :return:
+        '''
+
+        obj.delete()
+
+        # 重新生成新的列表静态界面
+        generate_static_list_search_html.delay()
+        # 先生成静态页面,后修改数据的话,永远慢一步, 必须先改数据,后生成
+
+
+
+# 如果 商品频道里的数据 改了,也需要重新生成
+class GoodsChannelAdmin(admin.ModelAdmin):
+    '''商品类别模型站点管理类'''
+    def save_model(self, request, obj, form, change):
+
+        obj.save()
+
+        # 重新生成新的列表静态界面
+        generate_static_list_search_html.delay()
+
+
+    def delete_model(self, request, obj):
+
+        obj.delete()
+
+        # 重新生成新的列表静态界面
+        generate_static_list_search_html.delay()
+
+
+
+admin.site.register(models.GoodsCategory,GoodsCategoryAdmin)
+admin.site.register(models.GoodsChannel,GoodsChannelAdmin)
 admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
