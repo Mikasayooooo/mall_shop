22841a5b4a4a55b2fdedaf823c575ed06b531e06
lhl <1056205431@qq.com>
2020-11-25 19:51:55 +0800

    购物车修改之登录用户操作redis

--- mall/mall/apps/carts/views.py
+++ mall/mall/apps/carts/views.py
@@ -223,29 +223,31 @@ class CartView(APIView):
         except:
             user = None
 
+        # 创建响应对象 , 将响应放在前面,减少冗余代码
+        response = Response(serializer.data)
+
         # is_authenticated 判断匿名用户还是 登录用户(判断用户是否通过认证)
         if user and user.is_authenticated:
             '''登录用户修改redis购物车数据'''
 
             # 创建redis连接对象
             redis_conn = get_redis_connection('cart')
-            pl = redis_conn.pipeline()  # 创建管道
 
-            # 添加 如果添加到sku_id在hash中已经存在,需要做增量
-            # redis_conn.hincrby('cart_%d % user.id, sku_id, count)
-            # redis_conn.hincrby('cart_%d % user.id, sku_id_5, 3)
-            # 如果要添加的sku_id在hash字典中不存在,就是新增,如果存在,就会自动做增量计数再存储
-            pl.hincrby('cart_%d' % user.id, sku_id, count)
+            # 创建管道对象
+            pl = redis_conn.pipeline()
 
-            # 把勾选的商品sku_id存储到set集合中
+            # 覆盖sku_id 对应的count
+            pl.hset('cart_%d' % user.id, sku_id, count)
+
+            # 如果勾选,就把勾选的商品sku_id存储到set集合中
             if selected:
                 pl.sadd('selected_%d' % user.id, sku_id)
+            else:
+                # 如果未勾选,就把未勾选的商品sku_id从set集合中移除
+                pl.srem('selected_%d' % user.id,sku_id)
 
             # 执行管道
             pl.execute()
-
-            # 响应
-            # return Response(serializer.data,status=status.HTTP_201_CREATED)
 
         else:
             '''非登录用户修改cookie购物车数据'''
@@ -295,7 +297,7 @@ class CartView(APIView):
             # 设置cookie
             response.set_cookie('cart', cart_str)
 
-            return response
+        return response
 
 
     def delete(self,request):
